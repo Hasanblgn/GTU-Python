66358856a28732a50c512f172e12a3ca2a56d3ce
Hasanblgn <72406753+Hasanblgn@users.noreply.github.com>
2023-07-03 16:03:35 +0300

    Third question

---
+++ lab0-3.py
@@ -0,0 +1,8 @@
+def addition(num1, num2):
+    total = num1 + num2
+    return f"{num1} + {num2} equal = {total}"
+
+
+num1 = int(input("Fist number :"))
+num2 = int(input("Second number :"))
+print(addition(num1,num2))
