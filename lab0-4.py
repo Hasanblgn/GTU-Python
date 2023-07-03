31960748a4bf96e9f56d9870c12a1c4c43f7d584
Hasanblgn <72406753+Hasanblgn@users.noreply.github.com>
2023-07-03 16:05:35 +0300

    fourth question

---
+++ lab0-4.py
@@ -0,0 +1,7 @@
+def float_subtraction(num1,num2):
+    return f"Your result:", num1-num2
+
+num1 = float(input("First number:"))
+num2 = float(input("Second Number:"))
+
+print(float_subtraction(num1, num2))
